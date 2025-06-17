f427adb2de49cd4fbd10133c35e30f65fcb9ac70
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2025-06-17 11:34:20 -0500

    first commit

---
+++ src/main/java/org/example/arswnetworking/ArswNetworkingApplication.java
@@ -0,0 +1,13 @@
+package org.example.arswnetworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArswNetworkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArswNetworkingApplication.class, args);
+    }
+
+}
